a1269ac1396de557984ec79fffeb3a949fa0023e
junhyeongP <ggdre10@naver.com>
2021-10-29 11:33:50 +0900

    .

--- meoka/src/main/java/org/zerock/domain/BoardVO.java
+++ meoka/src/main/java/org/zerock/domain/BoardVO.java
@@ -2,55 +2,34 @@ package org.zerock.domain;
 
 import java.util.Date;
 
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
+@Data
 public class BoardVO {
-	private Integer num;
-	private String name;
-	private String title;
-	private String content;
-	private Integer readCount;
-	private Date writeDate;
-	public Integer getNum() {
-		return num;
-	}
-	public void setNum(Integer num) {
-		this.num = num;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Integer getReadCount() {
-		return readCount;
-	}
-	public void setReadCount(Integer readCount) {
-		this.readCount = readCount;
-	}
-	public Date getWriteDate() {
-		return writeDate;
-	}
-	public void setWriteDate(Date writeDate) {
-		this.writeDate = writeDate;
-	}
-	@Override
-	public String toString() {
-		return "BoardVO [num=" + num + ", name=" + name + ", title=" + title + ", content=" + content + ", readCount="
-				+ readCount + ", writeDate=" + writeDate + "]";
-	}
+	private Integer RECIPENO;
+	private String MEMBERID;
+	private Integer FOODNO;
+	private String RECIPENAME;
+	private Date REGDATE;
+	private String COOKINGTIME;
+	private String PORTION;
+	private Integer RANKNO;
+
+	/*
+	 * public Integer getNum() { return num; } public void setNum(Integer num) {
+	 * this.num = num; } public String getName() { return name; } public void
+	 * setName(String name) { this.name = name; } public String getTitle() { return
+	 * title; } public void setTitle(String title) { this.title = title; } public
+	 * String getContent() { return content; } public void setContent(String
+	 * content) { this.content = content; } public Integer getReadCount() { return
+	 * readCount; } public void setReadCount(Integer readCount) { this.readCount =
+	 * readCount; } public Date getWriteDate() { return writeDate; } public void
+	 * setWriteDate(Date writeDate) { this.writeDate = writeDate; }
+	 */
+	/*
+	 * @Override public String toString() { return "BoardVO [num=" + num + ", name="
+	 * + name + ", title=" + title + ", content=" + content + ", readCount=" +
+	 * readCount + ", writeDate=" + writeDate + "]"; }
+	 */
 
 }
